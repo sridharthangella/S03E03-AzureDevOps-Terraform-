76fcb1f171d383e870f25f478a973af9f468a9c8
SridharThangella <sridhar.thangella@gmail.com>
2023-04-18 11:33:13 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
         container_name = "tfstate"
         key = "terraform.tfstate"
         subscription_id = "c88267eb-7f60-4620-bdac-44c5da10f56e"
-     #  client_id       = "REDACTED"
-    #   client_secret   = "REDACTED"
+        client_id       = "9ea72ad0-0d53-4eff-9205-3d6272b1625c"
+        client_secret   = "6gP8Q~YJoD7y~1aqmYMAym6vj_kCeKQvr_n7yb3f"
         tenant_id       = "6a8de571-d16e-4a63-b23d-5710aee607ef"
     
     }
