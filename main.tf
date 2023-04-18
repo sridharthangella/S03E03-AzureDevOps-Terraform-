5000b78347fd03b927919cb63bb7cd9a22a3b351
SridharThangella <sridhar.thangella@gmail.com>
2023-04-18 11:19:32 +0530

    updated pipeline file

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ provider "azurerm" {
     version = "2.5.0"
     features {}
 }
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "tf_rg"
+        storage_account_name = "tfstoragesridhar"
+        container_name = "tfstate"
+        key = "terraform.tfstate"
+        subscription_id = "c88267eb-7f60-4620-bdac-44c5da10f56e"
+     #  client_id       = "REDACTED"
+    #   client_secret   = "REDACTED"
+        tenant_id       = "6a8de571-d16e-4a63-b23d-5710aee607ef"
+    
+    }
+}   
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "East US"
